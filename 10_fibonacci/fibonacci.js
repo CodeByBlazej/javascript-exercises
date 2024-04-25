8c7f660c5245e2261dbc9d8e692a6a46c514dc0f
CodeByBlazej <codebyblazej@gmail.com>
2024-04-25 22:24:24 +0100

    second attempt to solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+  let fib = [];
+for (let i = 0; i < x; i++){
+  x = [i - 1] + [i - 2]
+  
+  // fib.push(x)
+  // num = (num - 1) + (num - 2)
+}
 
+return fib;
 };
 
 // Do not edit below this line
